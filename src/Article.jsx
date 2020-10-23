ebd8e1008524b9209ba18a7021c63b43e18ab6ca
Yana <masatomo.yanagihara.jp@gmail.com>
2020-10-23 12:39:02 +0900

    トラハック: stateの使用とsetStateで値の変更

--- src/Article.jsx
+++ src/Article.jsx
@@ -4,6 +4,8 @@ const Article = (props) => {
   return (
     <div>
       <h2>{props.title}</h2>
+      <label htmlFor="check">公開状態：</label>
+      <input id="check" type="checkbox" checked={props.isPublished} onClick={() => props.toggle()}/>
     </div>
   )
 }
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -5,13 +5,27 @@ import Article from "./Article";
 class Blog extends React.Component {
   constructor(props) {
     super(props);
+
+    // stateはconstructor内に記述する
+    this.state = {
+      isPublished: false,
+    }
   }
+
+  // setStateは関数でラップするのが一般的
+  togglePublished = () => {
+    // stateを変更する時はsetStateを使用
+    this.setState({
+      isPublished: !this.state.isPublished
+    })
+  }
+
   render() {
     return(
       <div>
-        <Article title={"タイトル1"} />
-        <Article title={"タイトル2"} />
-        <Article title={"タイトル3"} />
+        <Article title={"Reactの使い方"} isPublished={this.state.isPublished} toggle={() => 
+          this.togglePublished()
+        }/>
       </div>
     )
   }
